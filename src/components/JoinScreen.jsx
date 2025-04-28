4dd662b7a3e6b777de36faa19deb96b4effd5062
Muhammad Bilal Zulfiqar <bilalzulfiqar34@gmail.com>
2025-04-28 14:46:19 +0500

    improvements

--- src/components/JoinScreen.jsx
+++ src/components/JoinScreen.jsx
@@ -18,14 +18,14 @@ export default function JoinScreen({ onJoin }) {
   };
 
   return (
-    <div className="flex items-center justify-center h-[100dvh] bg-gray-100 dark:bg-gray-900 relative" style={{ transition: 'all 0.15s ease' }}>
+    <div className="flex items-center justify-center h-[100dvh] bg-gray-100 transition-colors dark:bg-gray-900 relative">
       <motion.div
         initial={{ opacity: 0, scale: 0.96 }}
         animate={{ opacity: 1, scale: 1 }}
         transition={{ duration: 1, ease: "easeOut" }}
-        className="flex relative flex-col m-6 items-center justify-center p-10 py-20 md:p-20 rounded-2xl shadow-lg bg-white dark:bg-gray-800 gap-6 w-md"
+        className="flex relative flex-col m-6 items-center justify-center transition-colors p-10 py-20 md:p-20 rounded-2xl shadow-lg bg-white dark:bg-gray-800 gap-6 w-md"
       >
-        <h1 className="text-3xl font-bold text-gray-800 dark:text-white mb-2">
+        <h1 className="text-3xl font-bold transition-colors text-gray-800 dark:text-white mb-2">
           Live Global Chat
         </h1>
 
@@ -39,7 +39,8 @@ export default function JoinScreen({ onJoin }) {
           onKeyDown={(e) => {
             if (e.key === "Enter" && username.trim()) handleJoin();
           }}
-          className="w-full px-4 py-2 rounded-lg border border-gray-300 dark:border-gray-600 bg-gray-50 dark:bg-gray-700 text-gray-800 dark:text-gray-200 placeholder-gray-400 transition-[box-shadow] duration-300 dark:placeholder-gray-500 focus:outline-none focus:ring-4 focus:ring-blue-500/20"
+          className="transition-join-custom w-full px-4 py-2 rounded-lg border border-gray-300 dark:border-gray-600 bg-gray-50 dark:bg-gray-700 text-gray-800 dark:text-gray-200 placeholder-gray-400 dark:placeholder-gray-500 focus:outline-none focus:ring-4 focus:ring-blue-500/20"
+          // style={{ transition: "box-shadow 300ms ease" }}
           autoComplete="off"
           autoCorrect="off"
           autoCapitalize="off"
@@ -49,16 +50,16 @@ export default function JoinScreen({ onJoin }) {
         {/* {error && <div className="text-sm text-red-500 -mt-2">{error}</div>} */}
 
         <button
-  onClick={handleJoin}
-  disabled={loading}
-  className={`w-full px-4 py-2 cursor-pointer rounded-lg text-white shadow-lg transition duration-300 ${
-    loading
-      ? "bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600"
-      : "bg-gradient-to-r from-blue-600 via-blue-700 to-indigo-600 hover:bg-gradient-to-r hover:from-blue-700 hover:via-blue-800 hover:to-indigo-700"
-  } focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-50`}
->
-  {loading ? "Joining..." : "Join Chat"}
-</button>
+          onClick={handleJoin}
+          disabled={loading}
+          className={`w-full px-4 py-2 cursor-pointer rounded-lg text-white shadow-lg transition duration-300 ${
+            loading
+              ? "bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600"
+              : "bg-gradient-to-r from-blue-600 via-blue-700 to-indigo-600 hover:bg-gradient-to-r hover:from-blue-700 hover:via-blue-800 hover:to-indigo-700"
+          } focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-50`}
+        >
+          {loading ? "Joining..." : "Join Chat"}
+        </button>
 
         {error && (
           <div className="absolute bottom-12 text-sm text-red-500 -mt-2">
